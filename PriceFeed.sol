8a319f7d83fe946fb7291caebd11872555a49eb4
Peter Harrington <peter@Jocelyns-iMac.hsd1.ca.comcast.net>
2020-10-06 07:17:38 -0700

    PriceFeed now poking

--- PriceFeed.sol
+++ PriceFeed.sol
@@ -3,10 +3,15 @@
 
 pragma solidity ^0.6.0;
 
+interface RealizedVol {
+    function poke() external;
+}
+
 contract PriceFeed {
     int128 public currentPrice;  // 64x64 fixed point
     
-    function setPrice(int128 _price) public {
+    function setPrice(int128 _price, RealizedVol _rvol) public {
         currentPrice = _price;
+        _rvol.poke(); 
     }
 }
