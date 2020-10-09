425665a682f6eabea9fe10693a428f2c42c9d1d6
Peter Harrington <peter@Jocelyns-iMac.hsd1.ca.comcast.net>
2020-10-09 07:24:11 -0700

    RealizedVol now imports ql

--- RealizedVol.sol
+++ RealizedVol.sol
@@ -4,54 +4,25 @@
 
 pragma solidity ^0.6.0;
 
-// ABDK's fixed point library, has average, sqrt, power, etc. 
-import {ABDKMath64x64} from "https://github.com/abdk-consulting/abdk-libraries-solidity/blob/master/ABDKMath64x64.sol";
-
+// my stats library for 64x64 bit fixed point
+import {ql} from "https://github.com/pbharrin/bc-quant/blob/main/ql.sol";
 
 contract PriceFeed{
     int128 public currentPrice;
 }
 
-// TODO: test approvedFeed setable by creator only
-// TODO: extract out std() calculation to a library. 
-
 contract RealizedVol{
-    using ABDKMath64x64 for int128;
-    
+
     int128 public vol; // the current volatility (the output)
     uint256 public timeForUpdate = 1601609551;
     uint256 updateDelta = 60; // 86400;  // time delta in seconds until next update is allowed
     int128 prevPrice;
     int128[] returnsData;
-    uint8 bufferSize = 0;
     uint8 public bufferPos = 0; // buffer position 
     address approvedFeed = 0x51BfE6949fD75Fbf806C9Aa3Ac4af4a1EF64A382; // approved feeds
     address creator; 
     
     string public debugMsg = 'not updated';
-    
-    function std(int128[] memory buff) internal pure returns (int128) {  // TODO: move this to library
-        // compute mean of buffer 
-        int128 accum = 0;
-        for (uint i=0; i<buff.length; i++){
-            accum = ABDKMath64x64.add(accum, buff[i]);
-        }
-        // divide 
-        int128 denom = int128(buff.length) << 64;
-        int128 bufferMean = ABDKMath64x64.div(accum, denom);
-        
-        // compute dispersion from mean
-        accum = 0;
-        int128 delta;
-        int128 sqDispersion;
-        for (uint i=0; i<buff.length; i++){
-            delta = ABDKMath64x64.sub(bufferMean, buff[i]);
-            sqDispersion = ABDKMath64x64.mul(delta, delta);
-            accum = ABDKMath64x64.add(accum, sqDispersion);
-        }
-        // lastly divide the sum sqDispersion by the number of points
-        return ABDKMath64x64.sqrt(ABDKMath64x64.div(accum, denom));  //std
-    }
     
     constructor() public {
         creator = msg.sender;
@@ -76,22 +47,20 @@ contract RealizedVol{
         int128 currPrice = pf.currentPrice();  
         
         // compute returns from price 
-        if (prevPrice == 0 && bufferSize == 0){  // cold start
+        if (prevPrice == 0 && returnsData.length == 0){  // cold start
             debugMsg = 'update ran - cold start';
         } else {
             debugMsg = 'update ran - normal';
             // compute return (curr - prevPrice)/prevPrice
-            int128 delta = ABDKMath64x64.sub(currPrice, prevPrice);
-            int128 ret = ABDKMath64x64.div(delta, prevPrice);
+            int128 ret = ql.pct_change(currPrice, prevPrice);
             
-            if (bufferSize < 30){
-                bufferSize++;
+            if (returnsData.length < 30){
                 returnsData.push(ret);
             } else {
                 returnsData[bufferPos] = ret;
             }
 
-            // update buffer
+            // update buffer position
             bufferPos++; 
             if (bufferPos > 29){
                 bufferPos = 0;
@@ -99,7 +68,6 @@ contract RealizedVol{
         }
         prevPrice = currPrice; 
     }
-    
     
     // debug function
     function getBuffer(uint256 i) external view returns(int128){
@@ -113,8 +81,8 @@ contract RealizedVol{
         updatePriceBuffer();
         
         // compute volatility only if we have enough data points
-        if (bufferSize > 9){  
-            vol = std(returnsData);
+        if (returnsData.length > 9){  
+            vol = ql.std(returnsData);
         }
         
         // update timeForUpdate
