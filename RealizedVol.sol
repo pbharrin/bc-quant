12b73ec87a085bc2b566812df18f3d3b9b102c0b
Peter Harrington <peter@Jocelyns-iMac.hsd1.ca.comcast.net>
2020-10-08 18:12:56 -0700

    abstracted std out

--- RealizedVol.sol
+++ RealizedVol.sol
@@ -12,6 +12,8 @@ contract PriceFeed{
     int128 public currentPrice;
 }
 
+// TODO: test approvedFeed setable by creator only
+// TODO: extract out std() calculation to a library. 
 
 contract RealizedVol{
     using ABDKMath64x64 for int128;
@@ -19,19 +21,53 @@ contract RealizedVol{
     int128 public vol; // the current volatility (the output)
     uint256 public timeForUpdate = 1601609551;
     uint256 updateDelta = 60; // 86400;  // time delta in seconds until next update is allowed
-    int128 public prevPrice;
-    int128[30] returnsData;
+    int128 prevPrice;
+    int128[] returnsData;
     uint8 bufferSize = 0;
     uint8 public bufferPos = 0; // buffer position 
-    address approvedFeed = 0x98dc8aF7cc0E59347b27275841015D8eA5a29Ae1; // array to hold approved feeds
+    address approvedFeed = 0x51BfE6949fD75Fbf806C9Aa3Ac4af4a1EF64A382; // approved feeds
+    address creator; 
     
     string public debugMsg = 'not updated';
+    
+    function std(int128[] memory buff) internal pure returns (int128) {  // TODO: move this to library
+        // compute mean of buffer 
+        int128 accum = 0;
+        for (uint i=0; i<buff.length; i++){
+            accum = ABDKMath64x64.add(accum, buff[i]);
+        }
+        // divide 
+        int128 denom = int128(buff.length) << 64;
+        int128 bufferMean = ABDKMath64x64.div(accum, denom);
+        
+        // compute dispersion from mean
+        accum = 0;
+        int128 delta;
+        int128 sqDispersion;
+        for (uint i=0; i<buff.length; i++){
+            delta = ABDKMath64x64.sub(bufferMean, buff[i]);
+            sqDispersion = ABDKMath64x64.mul(delta, delta);
+            accum = ABDKMath64x64.add(accum, sqDispersion);
+        }
+        // lastly divide the sum sqDispersion by the number of points
+        return ABDKMath64x64.sqrt(ABDKMath64x64.div(accum, denom));  //std
+    }
+    
+    constructor() public {
+        creator = msg.sender;
+    }
+    
+    function setApprovedFeed(address _feed) external {
+        require(creator == msg.sender);
+        approvedFeed = _feed;
+    }
     
     // price feeds will trigger this
     function poke() external{
         //check if it is time for an update
-        require(now > timeForUpdate);
-        update();
+        if (now > timeForUpdate){
+            update();
+        }
     }
     
     function updatePriceBuffer() internal{
@@ -48,9 +84,11 @@ contract RealizedVol{
             int128 delta = ABDKMath64x64.sub(currPrice, prevPrice);
             int128 ret = ABDKMath64x64.div(delta, prevPrice);
             
-            returnsData[bufferPos] = ret;
             if (bufferSize < 30){
                 bufferSize++;
+                returnsData.push(ret);
+            } else {
+                returnsData[bufferPos] = ret;
             }
 
             // update buffer
@@ -62,28 +100,6 @@ contract RealizedVol{
         prevPrice = currPrice; 
     }
     
-    function computeVol() internal{  // TODO: abstract this out for reuse elsewhere
-        // compute mean of buffer 
-        int128 accum = 0;
-        for (uint i=0; i<bufferSize; i++){
-            accum = ABDKMath64x64.add(accum, returnsData[i]);
-        }
-        // divide 
-        int128 denom = int128(bufferSize) << 64;
-        int128 bufferMean = ABDKMath64x64.div(accum, denom);
-        
-        // compute dispersion from mean
-        accum = 0;
-        int128 delta;
-        int128 sqDispersion;
-        for (uint i=0; i<bufferSize; i++){
-            delta = ABDKMath64x64.sub(bufferMean, returnsData[i]);
-            sqDispersion = ABDKMath64x64.mul(delta, delta);
-            accum = ABDKMath64x64.add(accum, sqDispersion);
-        }
-        // lastly divide the sum sqDispersion by the number of points
-        vol = ABDKMath64x64.div(accum, denom);  //variance 
-    }
     
     // debug function
     function getBuffer(uint256 i) external view returns(int128){
@@ -98,10 +114,11 @@ contract RealizedVol{
         
         // compute volatility only if we have enough data points
         if (bufferSize > 9){  
-            computeVol();
+            vol = std(returnsData);
         }
         
         // update timeForUpdate
         timeForUpdate += updateDelta;
     }
 }
+
